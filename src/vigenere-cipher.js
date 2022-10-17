2eaff9fcc5c83258110746f52330fa1301327396
nikita <nega123497@mail.com>
2022-10-17 23:04:46 +0300

    feat:zxc

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,57 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(reverse = true){
+    this.reverse = reverse
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) { throw new Error('Incorrect arguments!') }
+    let alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    let result = []
+    let keyString = ''
+    while(keyString.length < message.length) {
+      keyString.length >= message.length? keyString.slice(0, message.length) : keyString = keyString + key
+    }
+    keyString = keyString.toUpperCase()
+    message = message.toUpperCase()
+    let j = 0
+
+    for( let i=0; i<message.length; i++){
+      if(alphabet.includes(message[i])){
+        let indexLetter = (alphabet.indexOf(message[i]) + alphabet.indexOf(keyString[j])) % alphabet.length
+        result.push(alphabet[indexLetter])
+        j+=1
+      } else {
+        result.push(message[i])
+      }
+    }
+    return this.reverse? result.join('') : result.reverse().join('')
+  }  
+
+  decrypt(message, key) {
+    if (!message || !key) { throw new Error('Incorrect arguments!') }
+    let alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    let result = []
+    let keyString = ''
+    while(keyString.length < message.length) {
+      keyString.length >= message.length? keyString.slice(0, message.length) : keyString = keyString + key
+    }
+
+    keyString = keyString.toUpperCase()
+    message = message.toUpperCase()
+    let j = 0
+
+    for( let i=0; i<message.length; i++){
+      if(alphabet.includes(message[i])){
+        let indexLetter = (alphabet.indexOf(message[i]) - alphabet.indexOf(keyString[j]) + alphabet.length) % alphabet.length
+        result.push(alphabet[indexLetter])
+        j+=1
+      } else {
+        result.push(message[i])
+      }
+    }
+    return this.reverse? result.join('') : result.reverse().join('')
   }
 }
 
